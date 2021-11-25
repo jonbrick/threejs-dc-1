71ab337cdf351c7d6904df21edee6fed7fbe9f62
Jon Brick <jonbrick09@gmail.com>
2021-11-25 14:23:36 -0800

    Spinning ball with 2 lights

--- src/script.js
+++ src/script.js
@@ -3,6 +3,11 @@ import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import * as dat from "dat.gui";
 
+// Loading - instatiate and create texture loader
+const textureLoader = new THREE.TextureLoader();
+
+const normalTexture = textureLoader.load("/textures/NormalMap.png");
+
 // Debug
 const gui = new dat.GUI();
 0;
@@ -13,10 +18,16 @@ const canvas = document.querySelector("canvas.webgl");
 const scene = new THREE.Scene();
 
 // Objects - The geometry... the physical shape... Cubes, Rectangles, Spheres
+const geometry = new THREE.SphereBufferGeometry(0.5, 64, 64);
 
 // Materials - The clothing... your skin... lots of different types
-const material = new THREE.MeshBasicMaterial();
-material.color = new THREE.Color(0xff0000);
+
+const material = new THREE.MeshStandardMaterial();
+material.metalness = 0.7;
+material.roughness = 0.2;
+material.normalMap = normalTexture;
+
+material.color = new THREE.Color(0x292929);
 
 // Mesh - Ties both of them together... pass in Geometry and Material... then add to the screen. 4 step process... Geometry -> Skin -> Mesh -> Scene
 const sphere = new THREE.Mesh(geometry, material);
@@ -28,6 +39,53 @@ pointLight.position.x = 2;
 pointLight.position.y = 3;
 pointLight.position.z = 4;
 scene.add(pointLight);
+
+// Light 2
+const pointLight2 = new THREE.PointLight(0xff0000, 2);
+// pointLight.position.x = 2;
+// pointLight.position.y = 3;
+// pointLight.position.z = 4;
+pointLight2.position.set(-1.86, 1, -1.65);
+pointLight2.intensity = 10;
+
+scene.add(pointLight2);
+
+const light1 = gui.addFolder("Light 1");
+
+light1.add(pointLight2.position, "x").min(-6).max(6).step(0.01);
+light1.add(pointLight2.position, "y").min(-3).max(3).step(0.01);
+light1.add(pointLight2.position, "z").min(-3).max(3).step(0.01);
+light1.add(pointLight2, "intensity").min(0).max(10).step(0.01);
+
+// Light helpers!!
+// const pointLightHelper2 = new THREE.PointLightHelper(pointLight2, 0.5);
+// scene.add(pointLightHelper2);
+
+// Light 3
+const pointLight3 = new THREE.PointLight(0xe1ff, 2);
+pointLight3.position.set(2.13, -3, -1.98);
+pointLight3.intensity = 6.8;
+
+scene.add(pointLight3);
+
+const light2 = gui.addFolder("Light 2");
+
+light2.add(pointLight3.position, "x").min(-6).max(6).step(0.01);
+light2.add(pointLight3.position, "y").min(-3).max(3).step(0.01);
+light2.add(pointLight3.position, "z").min(-3).max(3).step(0.01);
+light2.add(pointLight3, "intensity").min(0).max(10).step(0.01);
+
+const light2color = {
+  color: 0xff0000,
+};
+
+light2.addColor(light2color, "color").onChange(() => {
+  pointLight3.color.set(light2color.color);
+});
+
+// Light helpers!!
+// const pointLightHelper3 = new THREE.PointLightHelper(pointLight3, 0.5);
+// scene.add(pointLightHelper3);
 
 /**
  * Sizes
@@ -71,6 +129,7 @@ scene.add(camera);
  */
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
+  alpha: true,
 });
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
